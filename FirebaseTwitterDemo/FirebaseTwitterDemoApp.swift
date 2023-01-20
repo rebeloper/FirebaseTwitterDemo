e569195e1f49def245264808cf0acf0c0ca818f4
rebeloper <therebeloper@gmail.com>
2023-01-20 15:42:22 +0200

    Initial Commit

---
+++ FirebaseTwitterDemo/FirebaseTwitterDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseTwitterDemoApp.swift
+//  FirebaseTwitterDemo
+//
+//  Created by Alex Nagy on 20.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseTwitterDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
